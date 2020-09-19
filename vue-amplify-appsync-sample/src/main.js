020bae3548acd95ccd9c74d688e995e356479217
ArshAulakh <arshaulakhios@gmail.com>
2020-09-19 16:02:35 +0530

    Initialized Amplify.

--- vue-amplify-appsync-sample/src/main.js
+++ vue-amplify-appsync-sample/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
 
+Amplify.configure(aws_exports)
 createApp(App).mount('#app')
+
